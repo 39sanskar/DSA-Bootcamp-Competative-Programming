c229e6e2f6544e0cfb7b6a9cce7e4dc6a66a0c90
39sanskar <39sanskar@gmail.com>
2025-06-07 00:11:13 +0530

    Infinite-Loops

---
+++ Complete-Basics/Infinite-Loops.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  // Creating Infinite loop using while loop
+  while (1) {
+  }
+
+  // Creating Infinite loop using for loop
+  for (;;) {
+  }
+
+  // Creating Infinite loop using for loop
+  for (int i = 1;; ++i) {
+  }
+  /*
+   You dont put any condition or put true as a condition
+  */
+
+  for (int i = 1; true; ++i) {
+  }
+}
