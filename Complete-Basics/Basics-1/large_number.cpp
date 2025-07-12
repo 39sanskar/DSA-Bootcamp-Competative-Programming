e242ad9e6e76e706c83a48bfe4385fe68b8c4f2a
39sanskar <mishrasanskar496@gmail.com>
2025-07-12 21:35:47 +0530

    using ASCII value

---
+++ Complete-Basics/Basics-1/large_number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  string s;
+  cin >> s;
+  int last_digit = s[s.size() - 1] - '0';
+  cout << last_digit << endl;
+  /*
+  Convert Char to Int:
+- '0'
+  '6' - '0' = ASCII value of '6' (54) minus ASCII of '0' (48) = 6.
+  */
+  
+}
