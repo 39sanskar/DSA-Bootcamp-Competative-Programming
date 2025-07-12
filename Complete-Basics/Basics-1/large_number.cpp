a9e605341a970a860dc0bb5e398e4c2095dc3a81
39sanskar <mishrasanskar496@gmail.com>
2025-07-12 21:36:15 +0530

    using ASCII value

--- Complete-Basics/Basics-1/large_number.cpp
+++ Complete-Basics/Basics-1/large_number.cpp
@@ -4,9 +4,14 @@ using namespace std;
 int main(){
   string s;
   cin >> s;
-  int last_digit = s[s.size() - 1] - '0';
+  int last_digit = s[s.size() - 1] - '0';  // basically s[i] it returns the index.
   cout << last_digit << endl;
   /*
+  Get Last Character:
+  s[s.size() - 1]
+  Accesses the last character in the string.
+  For "456", s[2] = '6'.
+
   Convert Char to Int:
 - '0'
   '6' - '0' = ASCII value of '6' (54) minus ASCII of '0' (48) = 6.
