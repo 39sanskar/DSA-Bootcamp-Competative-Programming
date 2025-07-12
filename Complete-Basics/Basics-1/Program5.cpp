8b28cba15894ae734e3b4bf756a4d20d97c31cee
39sanskar <mishrasanskar496@gmail.com>
2025-07-12 18:29:06 +0530

    string-basics

---
+++ Complete-Basics/Basics-1/Program5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+  string str = "Hello";
+  cout << str << endl;
+  string str2;
+  cin >> str2;
+  cout << str2 << endl;
+   
+}
