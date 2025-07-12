11080cfe1d4e1567e6499a217930ee2ee21367f7
39sanskar <mishrasanskar496@gmail.com>
2025-07-12 18:56:26 +0530

    string-basics

--- Complete-Basics/Basics-1/Program5.cpp
+++ Complete-Basics/Basics-1/Program5.cpp
@@ -6,7 +6,7 @@ int main(){
   string str = "Hello";
   cout << str << endl;
   string str2;
-  cin >> str2;
+  cin >> str2; // it will be break in the space or newline.
   cout << str2 << endl;
-   
+ 
 }
